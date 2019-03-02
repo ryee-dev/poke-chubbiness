3662d2d4f48f1aed559802e98a16fe85589404ae
roger yee <ryee.epicodus@gmail.com>
2019-03-01 17:45:18 -0800

    adds uri and samples query of 50 pokemon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient from 'apollo-boost';
+import gql from "graphql-tag";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 3rem 5rem;
+  }
+`;
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon.now.sh/"
+  // uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
+
+client
+.query({
+  query: gql`
+    {
+      pokemons(first: 50) {
+        name
+        id
+      }
+    }
+  `
+})
+.then(result => console.log(result));
+
+const ChubbyPokemon = () => (
+  <Query query={gql`
+  {
+    pokemons(first: 50) {
+      id
+      name
+      image
+    }
+  }
+  `}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.pokemons.map(({name, id, image}) => (
+        <div key={id}>
+          <div style={{ display: "flex", alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <p>{name}</p>
+            <img src={image} alt={name} style={{ padding: '1rem' }} />
+          </div>
+        </div>
+      ));
+    }}
+  </Query>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <GlobalStyle />
+        <h2>Chubby Pokemon</h2>
+        <ChubbyPokemon />
+      </ApolloProvider>
     );
   }
 }
